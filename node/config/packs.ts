78f4a779c548a969c9439e7822bed9c23bac0b24
SteekHutsy <45296796+SteekHutsy@users.noreply.github.com>
2024-01-26 23:07:33 +0100

    parole, frasi, palette e testa di ciccione
    sfaccimm'

---
+++ node/config/packs.ts
@@ -0,0 +1,283 @@
+interface Pack {
+    type: string,
+    nouns: string[],
+    verbs: string[]
+}
+
+const packs: Pack[] =
+    [
+        {
+            "type": "animals",
+            "nouns": [
+                "frog",
+                "horse",
+                "dog"
+            ],
+            "verbs": [
+                "shit",
+                "scream",
+                "owls"
+            ]
+        },
+    
+        {
+            "type": "animals2",
+            "nouns": [
+                "lion",
+                "elephant",
+                "penguin"
+            ],
+            "verbs": [
+                "roar",
+                "trumpet",
+                "waddle"
+            ]
+        },
+        {
+            "type": "jail",
+            "nouns": [
+                "inmate",
+                "cell",
+                "warden"
+            ],
+            "verbs": [
+                "confine",
+                "guards",
+                "restrict"
+            ]
+        },
+        {
+            "type": "hands",
+            "nouns": [
+                "finger",
+                "palm",
+                "thumb"
+            ],
+            "verbs": [
+                "slap",
+                "gesture",
+                "grab"
+            ]
+        },
+        {
+            "type": "holy",
+            "nouns": [
+                "sanctuary",
+                "jesus",
+                "altar"
+            ],
+            "verbs": [
+                "bless",
+                "praise",
+                "purify"
+            ]
+        },
+        {
+            "type": "monkey",
+            "nouns": [
+                "chimpanzee",
+                "gorilla",
+                "baboon"
+            ],
+            "verbs": [
+                "swing",
+                "chatter",
+                "climb"
+            ]
+        },
+        {
+            "type": "slurs",
+            "nouns": [
+                "asshole",
+                "dick",
+                "bitch"
+            ],
+            "verbs": [
+                "fuck",
+                "suck",
+                "blow"
+            ]
+        },
+        {
+            "type": "napoletano",
+            "nouns": [
+                "scugnizz'",
+                "cazzimma",
+                "'o vesuvio"
+            ],
+            "verbs": [
+                "sfaccimm",
+                "simmo",
+                "sta"
+            ]
+        },
+        {
+            "type": "napoletano2",
+            "nouns": [
+                "madre",
+                "patria",
+                "onore"
+            ],
+            "verbs": [
+                "comanda",
+                "ordina",
+                "rispetta"
+            ]
+        },
+        {
+            "type": "aliens",
+            "nouns": [
+                "ET",
+                "ufo",
+                "probes"
+            ],
+            "verbs": [
+                "abduct",
+                "invade",
+                "analyze"
+            ]
+        },
+        {
+            "type": "napoletanoGangster",
+            "nouns": [
+                "capa",
+                "'o mandamento",
+                "'o picciotto"
+            ],
+            "verbs": [
+                "fa 'o boss",
+                "scassà",
+                "'nfama"
+            ]
+        },
+        {
+            "type": "gangsta",
+            "nouns": [
+                "homie",
+                "ride",
+                "stash"
+            ],
+            "verbs": [
+                "represent",
+                "hustle",
+                "ballin'"
+            ]
+        },
+        {
+            "type": "feet",
+            "nouns": [
+                "toes",
+                "soles",
+                "arch"
+            ],
+            "verbs": [
+                "walking",
+                "stomp",
+                "tiptoe"
+            ]
+        },
+        {
+            "type": "student",
+            "nouns": [
+                "notebook",
+                "exam",
+                "lecture"
+            ],
+            "verbs": [
+                "study",
+                "collaborate",
+                "attend"
+            ]
+        },
+        {
+            "type": "illness",
+            "nouns": [
+                "ache",
+                "flu",
+                "cure"
+            ],
+            "verbs": [
+                "feel",
+                "treat",
+                "heal"
+            ]
+        },
+        {
+            "type": "disgust",
+            "nouns": [
+                "slime",
+                "odor",
+                "gunk"
+            ],
+            "verbs": [
+                "ooze",
+                "reeks",
+                "repulse"
+            ]
+        },
+        {
+            "type": "plants",
+            "nouns": [
+                "flower",
+                "leaf",
+                "root"
+            ],
+            "verbs": [
+                "bloom",
+                "die",
+                "grow"
+            ]
+        },
+        {
+            "type": "evil",
+            "nouns": [
+                "darkness",
+                "demon",
+                "curse"
+            ],
+            "verbs": [
+                "haunt",
+                "corrupt",
+                "hunt"
+            ]
+        },
+        {
+            "type": "mafia",
+            "nouns": [
+                "don",
+                "consigliere",
+                "racket"
+            ],
+            "verbs": [
+                "extort",
+                "silence",
+                "control"
+            ]
+        },
+        {
+            "type": "jester",
+            "nouns": [
+                "fool",
+                "jingle cap",
+                "prank"
+            ],
+            "verbs": [
+                "jest",
+                "entertain",
+                "mischief-make"
+            ]
+        },
+        {
+            "type": "violence",
+            "nouns": [
+                "fist",
+                "gun",
+                "rifle"
+            ],
+            "verbs": [
+                "slap",
+                "kill",
+                "beat"
+            ]
+        },
+    ]
---
+++ node/config/riddles.ts
@@ -0,0 +1,29 @@
+interface Riddle {
+    riddles: string[],
+   
+}
+
+const riddles: Riddle[] =
+    [
+        {
+            "riddles": [
+            "What gives life its meaning for you?",
+            "What aspects of life spark your deepest sense of wonder?",
+            "What's the best cook book you know?",
+            "Worst thing about italians?",
+            "If i could i would kill all",
+            
+            ]
+        }
+
+
+
+
+
+
+
+
+
+
+    ]
+    
