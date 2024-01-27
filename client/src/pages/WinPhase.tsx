aa8ab7dfb7a8eda0265cebdec716601bad7df5e6
trisogene <trisogene@gmail.com>
2024-01-27 17:31:04 +0100

    fix umbe

--- client/src/pages/WinPhase.tsx
+++ client/src/pages/WinPhase.tsx
@@ -1,16 +1,17 @@
-import WithMotion from "src/hoc/WithMotion";
 import { Choice } from "src/types";
 
 interface WinPhaseProps {
   winningChoice?: Choice;
+  onReadyForNextRound: () => void;
 }
 
-const WinPhase = ({ winningChoice }: WinPhaseProps) => {
+const WinPhase = ({ winningChoice, onReadyForNextRound }: WinPhaseProps) => {
   return (
     <div>
       <h1>Win Phase</h1>
       <h3>{winningChoice?.choice}</h3>
+      <button onClick={onReadyForNextRound}>Next Round</button>
     </div>
   );
 };
-export default WithMotion(WinPhase);
+export default WinPhase;
